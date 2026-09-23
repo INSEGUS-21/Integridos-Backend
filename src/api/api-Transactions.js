e781a7ed94c62f7650a44798a8f4ec1b26592b39
ivan7112 <ivaarrdom@alum.us.es>
2026-09-23 20:55:13 +0200

    backend working

--- src/api/api-Transactions.js
+++ src/api/api-Transactions.js
@@ -1,9 +1,17 @@
-const mongoose = require('mongoose');
+
+import mongoose from 'mongoose';
 
 const Schema=mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 const BASE_URL="/api/v1";
 
+const MONGO_URL="mongodb://localhost:27017/";
+
+try{
+    await mongoose.connect(MONGO_URL).then(() => console.log('Connected!'));
+}catch(error){
+     console.log("El error es: ", error);
+}
 
 const Transaction_scheme=new Schema(
     {
@@ -24,11 +32,7 @@ await Transaction_model.createCollection();
 
 export const loadTransactionApi=async  (app) =>{
 
-    try{
-       await mongoose.connect('mongodb://127.0.0.1:27017/test').then(() => console.log('Connected!'));
-    }catch(error){
-        console.log(error);
-    }
+    
 
     app.get(BASE_URL+"/transactions",async  (req,res)=>{
         const data= await Transaction_model.find({}).select("-_id");
@@ -39,7 +43,8 @@ export const loadTransactionApi=async  (app) =>{
     });
 
     app.get(BASE_URL+"/transactions/:id", async (req,res)=>{
-        const data=await Transaction_model.findOne({tx_id:req.params.id}).select("-_id");
+        const id=new ObjectId(req.params.id);
+        const data=await Transaction_model.findOne({tx_id:id}).select("-_id");
         const json=await data.json();
        res.status(200).send(json);
     })
@@ -57,7 +62,8 @@ export const loadTransactionApi=async  (app) =>{
     });
 
     app.delete(BASE_URL+"/transactions/:id", async (req, res)=>{
-        await Transaction_model.deleteOne({tx_id:req.params.id});
+        const id=new ObjectId(req.params.id);
+        await Transaction_model.deleteOne({tx_id:id});
         res.sendStatus(200);
     });
 
