1eb7693b72770b277fce7323fdfb5ec3886d0d09
QXB8295 <martorcha@alum.us.es>
2026-09-24 11:03:00 +0200

    typo correction

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { loadBackendApiUsers } from './api/api-Users.js'; 
+import { loadBackendApiUsers } from './src/api-Users.js'; 
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors()); 
 app.use(express.json());
 loadBackendApiUsers(app);
 
