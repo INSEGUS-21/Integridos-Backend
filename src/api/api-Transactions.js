c063dc11389bd2b2fd0927b205c8f56d109df728
danieldlrf <danieldlrf@gmail.com>
2026-09-24 13:23:50 +0200

    feature: added service nonce to the transaction api and added transaction api errors

--- src/api/api-Transactions.js
+++ src/api/api-Transactions.js
@@ -1,5 +1,6 @@
 
 import mongoose from 'mongoose';
+import { createNonce } from '../services/serviceNonce';
 
 const Schema=mongoose.Schema;
 const ObjectId = Schema.ObjectId;
@@ -20,9 +21,8 @@ const Transaction_scheme=new Schema(
         destination_account:String,
         amount:Number,
         currency:String,
-        timestamp:Number
-
-
+        timeStamp:Number,
+        nonce:String
     }
 );
 
@@ -35,41 +35,76 @@ export const loadTransactionApi=async  (app) =>{
     
 
     app.get(BASE_URL+"/transactions",async  (req,res)=>{
-        const data= await Transaction_model.find({}).select("-_id");
+        try {
+            const data= await Transaction_model.find({}).select("-_id");
 
-        const json=await data.json();
+            const json=await data.json();
 
-        res.status(200).send(json);
+            return res.status(200).send(json);
+        } catch (error) {
+            return res.sendStatus(500)
+        }
+        
     });
 
     app.get(BASE_URL+"/transactions/:id", async (req,res)=>{
-        const id=new ObjectId(req.params.id);
-        const data=await Transaction_model.findOne({tx_id:id}).select("-_id");
-        const json=await data.json();
-       res.status(200).send(json);
+        const id=new ObjectId(req.params.id); 
+        try {
+            const data=await Transaction_model.findOne({tx_id:id}).select("-_id");
+            if (data){
+                const json=await data.json();
+                return res.status(200).send(json); 
+            }else{
+                return res.sendStatus(404)
+            }
+        } catch (error) {
+            return res.sendStatus(500)   
+        }        
     })
 
     app.post(BASE_URL+"/transactions", async (req,res)=>{
-        //Aquí han de implementarse mecanismos de seguridad
-
-        Transaction_model.insertOne(req.body);
-        res.sendStatus(200);
+        const nonce = req.body.nonce;
+        const timeStamp = req.body.timeStamp;
+        try {
+            if (validNonce(nonce, timeStamp)){
+                if (createNonce(nonce,timeStamp)){
+                    await Transaction_model.insertOne(req.body);
+                    return res.sendStatus(200);
+                } else {
+                    return res.sendStatus(400)
+                }
+            }else{
+                return res.sendStatus(400)
+            }
+        } catch (error) {
+            return res.sendStatus(500);
+        }
+        
     });
 
     app.delete(BASE_URL+"/transactions", async (req,res)=>{
-        await Transaction_model.deleteMany({});
-        res.sendStatus(200);
+        try {
+            await Transaction_model.deleteMany({});
+            return res.sendStatus(200);
+        } catch (error) {
+            return res.sendStatus(500);
+        }
+        
     });
 
     app.delete(BASE_URL+"/transactions/:id", async (req, res)=>{
-        const id=new ObjectId(req.params.id);
-        await Transaction_model.deleteOne({tx_id:id});
-        res.sendStatus(200);
+        const id=new ObjectId(req.params.id); 
+        try {
+            await Transaction_model.deleteOne({tx_id:id});
+            return res.sendStatus(200); 
+        } catch (error) {
+            return res.sendStatus(500)   
+        }    
     });
 
 
     app.post(BASE_URL+"/transactions:id", (req,res)=>{
-        res.sendStatus(405);
+        return res.sendStatus(405);
     });
 
 
