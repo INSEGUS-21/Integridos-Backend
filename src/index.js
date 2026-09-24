c52f46984ae12162de1a00f47ca66b597144d42a
danieldlrf <danieldlrf@gmail.com>
2026-09-24 18:11:47 +0200

    feature: render

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { loadBackendApiUsers } from './api/api-Users.js';
 import { loadTransactionApi } from './api/api-Transactions.js';
 
 const app=express()
-const PORT=3000;
+const PORT= process.env.PORT ||3000;
 
 app.use(cors()); 
 app.use(express.json({
@@ -19,5 +19,5 @@ loadTransactionApi(app);
 loadBackendApiUsers(app);
 
 app.listen(PORT, () => {
-    console.log("Backend Now Running");
+    console.log(`Backend Now Running on the port ${PORT}`);
 });
